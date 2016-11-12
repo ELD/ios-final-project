a0217c2c8e3ff1fe0704b89eb1be6971478eb86c
Ethan Bonin <ethanbonin@gmail.com>
2016-11-12 10:57:19 -0700

    Got the some of the transitions to start working..

---
+++ SocialJoint/GradientExtension.swift
@@ -0,0 +1,41 @@
+import UIKit
+extension CAGradientLayer {
+    
+    
+    func turquoiseColor() -> CAGradientLayer {
+        let topColor = UIColor(red: (15/255.0), green: (118/255.0), blue: (128/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (84/255.0), green: (187/255.0), blue: (187/255.0), alpha: 1)
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        return gradientLayer
+    }
+    
+    func WhiteGrayColor() -> CAGradientLayer {
+        let topColor = UIColor(red: (113/255.0), green: (113/255.0), blue: (113/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (33/255.0), green: (33/255.0), blue: (33/255.0), alpha: 1)
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        return gradientLayer
+    }
+    
+    
+    func IntroColor() -> CAGradientLayer {
+        let topColor = UIColor(red: (44/255.0), green: (166/255.0), blue: (147/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (33/255.0), green: (75/255.0), blue: (110/255.0), alpha: 1)
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        return gradientLayer
+    }
+    
+    
+    
+}
--- SocialJoint/InitialSetUpVC.swift
+++ SocialJoint/InitialSetUpVC.swift
@@ -8,50 +8,139 @@
 
 import UIKit
 
-class InitialSetUpVC: UIViewController {
+class InitialSetUpVC: UIViewController, CAAnimationDelegate {
     
     
     // OUTLETS //
     @IBOutlet var WelcomeLabel: UILabel!
-    @IBOutlet var BackGrounView: UIView!
     
     
     //VARIABLES//
     
     //This is to keep track of how many times Handle Tap occurs
     var introScreenTracker: Int = 0
+    let aSelectorFirst : Selector = #selector(InitialSetUpVC.HandleFirstTap)
+    let aSelectorSecond : Selector = #selector(InitialSetUpVC.HandleSecondTap)
+    var tapGesture = UITapGestureRecognizer()
     
     
-    override func viewDidLoad() {
-        BackGrounView.isUserInteractionEnabled = true;
-        let aSelector : Selector = "HandleTap"
-        let tapGesture = UITapGestureRecognizer(target: self, action: aSelector)
-        BackGrounView.addGestureRecognizer(tapGesture)
+    var gradient : CAGradientLayer?;
+    
+    
+    override  func viewDidLoad() {
+        super.viewDidLoad()
+        view.isUserInteractionEnabled = true;
         
-    }
-    
-    
-    func HandleTap(){
-        //When tapped the Animation fades out
         
-        if (introScreenTracker == 0){
-            UIView.animate(withDuration: 2) {
-                self.WelcomeLabel.alpha = 0
-                if (self.WelcomeLabel.alpha == 0){
-                    self.WelcomeLabel.text = "Where all Social Platforms Meet \nOne Place"
-                    UIView.animate(withDuration: 2, animations: {
-                        self.WelcomeLabel.alpha = 1
-                        self.introScreenTracker = self.introScreenTracker + 1
-                    })
-                }
-            }
+        if (introScreenTracker == 0) {
+            tapGesture = UITapGestureRecognizer(target: self, action: aSelectorFirst)
+            view.addGestureRecognizer(tapGesture)
+        }
+        if (introScreenTracker == 1) {
+            
+            tapGesture = UITapGestureRecognizer(target: self, action: aSelectorSecond)
+            view.addGestureRecognizer(tapGesture)
+            
         }
         
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.gradient = CAGradientLayer().IntroColor()
+        self.gradient?.frame = self.view.bounds
+        self.view.layer.insertSublayer(self.gradient!, at: 0)
+    }
+    
+    func introAnimateLayer(){
+        
+        let fromColors = self.gradient?.colors
+        
+        let topColor = UIColor(red: (3/255.0), green: (75/255.0), blue: (110/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (95/255.0), green: (148/255.0), blue: (174/255.0), alpha: 1)
+        
+        let toColors: [AnyObject] = [topColor.cgColor, bottomColor.cgColor]
+        self.gradient?.colors = toColors
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "colors")
+        
+        //THIS PERFORMS THE TRANSITION
+        animation.fromValue = fromColors
+        animation.toValue = toColors
+        animation.duration = 3.00
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = kCAFillModeForwards
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.delegate = self
+        
+        self.gradient?.add(animation, forKey:"animateGradient")
+    }
+    
+    func secondAnimateLayer() {
+        
+        let fromColors = self.gradient?.colors
+        
+        //TOP AND BOTTOM COLOR FOR FIRST TRANSITION
+        let topColor = UIColor(red: (95/255.0), green: (148/255.0), blue: (174/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (195/255.0), green: (222/255.0), blue: (235/255.0), alpha: 1)
+        
+        let toColors: [AnyObject] = [topColor.cgColor, bottomColor.cgColor]
+        self.gradient?.colors = toColors
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "colors")
+        
+        //THIS PERFORMS THE TRANSITION
+        animation.fromValue = fromColors
+        animation.toValue = toColors
+        animation.duration = 3.00
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = kCAFillModeForwards
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.delegate = self
+        
+        self.gradient?.add(animation, forKey:"animateGradient")
         
         
     }
     
     
+    func HandleFirstTap(){
+        //When tapped the Animation fades out
+        
+        introAnimateLayer()
+        UIView.animate(withDuration: 3) {
+            self.WelcomeLabel.alpha = 0
+            if (self.WelcomeLabel.alpha == 0){
+                self.WelcomeLabel.text = "Where all Social Platforms Meet \nOne Place"
+                UIView.animate(withDuration: 5, animations: {
+                    self.WelcomeLabel.alpha = 1
+                    
+                })
+            }
+        }
+        introScreenTracker = introScreenTracker + 1
+        
+        
+    }
+    
+    
+    func HandleSecondTap(){
+        //When tapped the Animation fades out
+        
+        secondAnimateLayer()
+        
+        UIView.animate(withDuration: 3) {
+            self.WelcomeLabel.alpha = 0
+            if (self.WelcomeLabel.alpha == 0){
+                self.WelcomeLabel.text = "Choose your Social Media\n that you would like to See"
+                UIView.animate(withDuration: 5, animations: {
+                    self.WelcomeLabel.alpha = 1
+                    
+                })
+            }
+        }
+        introScreenTracker = introScreenTracker + 1
+        
+        
+    }
     
     
     override func didReceiveMemoryWarning() {
