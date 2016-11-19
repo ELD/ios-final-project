495ab46c693c6d82e75302f6a7cf60703a36beac
Eric Dattore <edattore@gmail.com>
2016-11-19 15:18:27 -0700

    add Facebook login button/capabilities

--- SocialJoint/SignInVC.swift
+++ SocialJoint/SignInVC.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import FacebookLogin
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, LoginButtonDelegate {
 
     
     
@@ -16,11 +17,35 @@ class SignInVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        
+        loginButton.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        print("Did log out of Facebook")
+    }
+    
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+//        public enum LoginResult {
+//            /// User succesfully logged in. Contains granted, declined permissions and access token.
+//            case success(grantedPermissions: Set<Permission>, declinedPermissions: Set<Permission>, token: AccessToken)
+//            /// Login attempt was cancelled by the user.
+//            case cancelled
+//            /// Login attempt failed.
+//            case failed(Error)
+//        }
+        switch result {
+        case .success(let granted, let declined, let token):
+            print("Login successful")
+        case .cancelled:
+            print("Login cancelled")
+        case .failed(let error):
+            print("Login failed: \(error)")
+        }
     }
     
 
